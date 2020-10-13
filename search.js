b14d97792061fcf52072108409a09895dd02f8f0
Yano <yano@yanoryuichi.com>
2020-10-13 12:50:02 +0900

    Added showing data

--- search.js
+++ search.js
@@ -1,5 +1,13 @@
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            books: []
+        }
+        this.search = this.search.bind(this);
+    }
+
     async search(evt) {
         evt.preventDefault();
         const form = evt.target;
@@ -7,12 +15,14 @@ class Search extends React.Component {
         const url = `https://www.googleapis.com/books/v1/volumes?q=${book}`
         const response = await fetch(url);
         const data = await response.json();
+        const books = [];
         data.items.map((item => {
             const id = item.id;
             const title = item.volumeInfo.title;
             const image = item.volumeInfo.imageLinks.thumbnail;
-            console.log({id, title, image});
+            books.push({ id, title, image });
         }));
+        this.setState({ books: books });
     }
 
     render() {
@@ -23,6 +33,18 @@ class Search extends React.Component {
                     <input name="book" type="text" />
                     <button type="submit">Search</button>
                 </form>
+                <div>
+                    {
+                        this.state.books.map((book) => {
+                            return (
+                            <div key={book.id}>
+                                <h2>{book.title}</h2>
+                                <img src={book.image}/>
+                            </div>
+                            );
+                        })
+                    }
+                </div>
             </div>
         );
     }
