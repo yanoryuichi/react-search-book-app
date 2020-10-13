2e01e233a0cad360e16cd908ec4b64249dac7248
Yano <yano@yanoryuichi.com>
2020-10-13 11:49:43 +0900

    Added search function

--- search.js
+++ search.js
@@ -1,12 +1,18 @@
 
 class Search extends React.Component {
+    search(evt) {
+        evt.preventDefault();
+        const form = evt.target;
+        alert(form.book.value);
+    }
+
     render() {
         return (
             <div>
                 <h1>Search</h1>
-                <form>
-                    <input type="text" />
-                    <button>Search</button>
+                <form onSubmit={this.search}>
+                    <input name="book" type="text" />
+                    <button type="submit">Search</button>
                 </form>
             </div>
         );
